aee1764fd7f3cb949c01f4c6d7a1fe52a629ee9a
Neaz-mq <neazmorshed666@gmail.com>
2024-01-05 00:43:22 +0600

    firebase setup and login

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,17 +1,45 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+
 
 const NavBar = () => {
+    const { user, logOut } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.log(error));
+    }
 
     const navOptions = <>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Our Menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
-        
+        <li><Link to="/secret">Secret</Link></li>
+        {
+            user ? <>
+            
+                
+                <button onClick={handleLogOut} className="btn btn-ghost -mt-2">LogOut</button>
+
+                
+                <img
+                    src={user?.photoURL}  
+                    alt="User"
+                    className="rounded-full h-9 w-9 mr-2 ml-8"
+                />
+                <span className="font-bold text-xl text-white-400 mt-1">{user?.displayName}</span>
+                
+            </> : <>
+                <li><Link to="/login">Login</Link></li>
+            </>
+        }
     </>
 
     return (
         <>
-            <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-teal-400 text-white">
+            <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -21,7 +49,7 @@ const NavBar = () => {
                             {navOptions}
                         </ul>
                     </div>
-                    <a href="/" className="btn btn-ghost normal-case text-xl">Delicious Food</a>
+                    <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
