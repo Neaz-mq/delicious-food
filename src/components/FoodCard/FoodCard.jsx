d0b01cf38b364094367b29558afc9cb2ea948748
Neaz-mq <neazmorshed666@gmail.com>
2024-01-08 11:24:34 +0600

    working with tanstack query

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,6 +1,7 @@
 import Swal from "sweetalert2";
 import useAuth from "../../hooks/useAuth";
 import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 
 const FoodCard = ({item}) => {
@@ -8,50 +9,53 @@ const FoodCard = ({item}) => {
     const {user} = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
+    const axiosSecure = useAxiosSecure();
 
-    const handleAddToCart = food => {
+    const handleAddToCart = () => {
     
-       
-        if(user && user.email){
-            const cartItem = {menuItemId: _id, name, image, price, email: user.email}
-            fetch('http://localhost:5000/carts', {
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify(cartItem)
-            })
-            .then(res => res.json())
-            .then(data => {
-                if(data.insertedId){
-                   
-                    Swal.fire({
-                        position: 'top-end',
-                        icon: 'success',
-                        title: 'Food added on the cart.',
-                        showConfirmButton: false,
-                        timer: 1500
-                      })
-                }
-            })
+        if (user && user.email) {
+            //send cart item to the database
+            const cartItem = {
+                menuId: _id,
+                email: user.email,
+                name,
+                image,
+                price
+            }
+            axiosSecure.post('/carts', cartItem)
+                .then(res => {
+                    console.log(res.data)
+                    if (res.data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: `${name} added to your cart`,
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                        // refetch cart to update the cart items count
+                       
+                    }
+
+                })
         }
-        else{
+        else {
             Swal.fire({
-                title: 'Please login to order the food',
-                icon: 'warning',
+                title: "You are not Logged In",
+                text: "Please login to add to the cart?",
+                icon: "warning",
                 showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Login now!'
-              }).then((result) => {
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, login!"
+            }).then((result) => {
                 if (result.isConfirmed) {
-                  navigate('/login', {state: {from: location}})
+                    //   send the user to the login page
+                    navigate('/login', { state: { from: location } })
                 }
-              })
+            });
         }
     }
-    
-
 
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
