5dbef635a7b93cf52f04c3e864fd6033d68db1e5
Neaz-mq <neazmorshed666@gmail.com>
2024-01-17 21:47:15 +0600

    filter by price

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -13,6 +13,7 @@ const Order = () => {
     const { category } = useParams();
     const initialIndex = categories.indexOf(category);
     const [tabIndex, setTabIndex] = useState(initialIndex);
+    const [sortBy, setSortBy] = useState('highToLow');
     const [menu] = useMenu();
     
     const desserts = menu.filter(item => item.category === 'dessert');
@@ -21,6 +22,10 @@ const Order = () => {
     const pizza = menu.filter(item => item.category === 'pizza');
     const drinks = menu.filter(item => item.category === 'drinks');
 
+    const handleSortChange = () => {
+        setSortBy(sortBy === 'highToLow' ? 'lowToHigh' : 'highToLow');
+    };
+
     return (
         <div>
             <Helmet>
@@ -28,27 +33,37 @@ const Order = () => {
             </Helmet>
             <Cover img={orderCoverImg} title="Order Food"></Cover>
             <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                <TabList>
-                    <Tab>Salad</Tab>
-                    <Tab>Pizza</Tab>
-                    <Tab>Soup</Tab>
-                    <Tab>Dessert</Tab>
-                    <Tab>Drinks</Tab>
+            <TabList className="flex mb-10">
+                    {categories.map((cat, index) => (
+                        <Tab key={index} className={`mr-4 mt-4 p-5 cursor-pointer ${index === tabIndex ? 'bg-orange-500 text-white' : 'bg-gray-300 text-gray-700 hover:bg-gray-400 hover:text-gray-800'} rounded-md`}>
+                        <div className="flex items-center">
+                            <div className="inline-block">
+                                {cat}
+                            </div>
+                            <button
+                                onClick={handleSortChange}
+                                className={`ml-4 p-2 rounded-md text-xs ${sortBy === 'highToLow' ? 'bg-red-500 text-white' : 'bg-green-500 text-white'}`}
+                            >
+                                {sortBy === 'highToLow' ? 'High to Low Price' : 'Low to High Price'}
+                            </button>
+                        </div>
+                    </Tab>
+                    ))}
                 </TabList>
                 <TabPanel>
-                    <OrderTab items={salad}></OrderTab>
+                <OrderTab items={salad} sortBy={sortBy}></OrderTab>
                 </TabPanel>
                 <TabPanel>
-                    <OrderTab items={pizza}></OrderTab>
+                    <OrderTab items={pizza} sortBy={sortBy}></OrderTab>
                 </TabPanel>
                 <TabPanel>
-                    <OrderTab items={soup}></OrderTab>
+                    <OrderTab items={soup} sortBy={sortBy}></OrderTab>
                 </TabPanel>
                 <TabPanel>
-                    <OrderTab items={desserts}></OrderTab>
+                    <OrderTab items={desserts} sortBy={sortBy}></OrderTab>
                 </TabPanel>
                 <TabPanel>
-                    <OrderTab items={drinks}></OrderTab>
+                    <OrderTab items={drinks} sortBy={sortBy}></OrderTab>
                 </TabPanel>
             </Tabs>
         </div>
@@ -56,3 +71,7 @@ const Order = () => {
 };
 
 export default Order;
+
+
+
+
--- src/pages/Order/OrderTab/OrderTab.jsx
+++ src/pages/Order/OrderTab/OrderTab.jsx
@@ -3,38 +3,34 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
-import { Pagination } from 'swiper/modules';
 
-const OrderTab = ({ items }) => {
-    const pagination = {
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
-    };
+
+const OrderTab = ({ items, sortBy }) => {
+    const sortedItems = [...items];
+    if (sortBy === 'highToLow') {
+        sortedItems.sort((a, b) => b.price - a.price);
+    } else {
+        sortedItems.sort((a, b) => a.price - b.price);
+    }
+
+   
     return (
         <div >
 
-            <Swiper
-                pagination={pagination}
-                modules={[Pagination]}
+<Swiper
+                pagination={{ clickable: true, renderBullet: (index, className) => `<span class="${className}">${index + 1}</span>` }}
                 className="mySwiper"
             >
                 <SwiperSlide>
                     <div className='grid md:grid-cols-3 gap-10'>
-                        {
-                            items.map(item => <FoodCard
-                                key={item._id}
-                                item={item}
-                            ></FoodCard>)
-                        }
+                        {sortedItems.map(item => <FoodCard key={item._id} item={item}></FoodCard>)}
                     </div>
-
                 </SwiperSlide>
-
             </Swiper>
         </div>
     );
 };
 
 export default OrderTab;
+
+
